a8c12cc60677820b38499a135e652679b9bda361
Martin Ueding <dev@martin-ueding.de>
2013-03-18 20:02:05 +0100

    Delete everything

--- note.php
+++
@@ -1,7 +0,0 @@
-<?PHP /* Copyright (c) 2011 Martin Ueding <dev@martin-ueding.de> */ ?>
-<form action="save_note.php" method="post">
-<textarea name="note"></textarea>
-<input type="submit" value="save" />
-<!-- TODO add checkboxes with tags -->
-<!-- TODO add text field for new tags -->
-</form>
