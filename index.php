5b899cb730cc8defacf51c31d9deeaf03ae3b4ca
Martin Ueding <dev@martin-ueding.de>
2013-10-02 11:25:19 +0200

    Remove php-authenticator

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 
 require_once('Notes.php');
 
-require_once('php-authenticator/Authenticator.php');
-
-$auth = new de\martin_ueding\authenticator\Authenticator();
-$auth->requireLogin();
-
 $notes = new Notes();
 
 if (isset($_POST['text'])) {
